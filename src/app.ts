7d21a6717b11d3e6ed368d5305035c130a78bbae
Imran-Ho <imranhossain36@gmail.com>
2023-11-23 01:47:31 -0600

    created config file and rejoin with mongoDB

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,7 @@
-const express = require("express");
-const app = express();
+import express, { Application, Request, Response } from "express";
+const app: Application = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
 });
