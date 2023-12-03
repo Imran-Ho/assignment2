8444988265768f50fd68213218ed334dc19191cd
Imran-Ho <imranhossain36@gmail.com>
2023-12-02 20:58:44 -0600

    new create user Endpoint api refixed

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -3,7 +3,7 @@ import { userController } from './user.controller';
 
 const router = express.Router();
 
-router.post('/new-user', userController.createNewUser);
+router.post('', userController.createNewUser);
 
 router.get('', userController.getDBExistingUsers);
 router.put('/:userId', userController.updatedUserInfo);
